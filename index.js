a381591d6b83148a10bcf98cc7e70d1398c24610
EriclesCalvt <ericlesprogrammer@gmail.com>
2023-04-20 12:35:06 -0300

    Script code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.querySelector("#locLavyne").addEventListener("click", () => {
     "https://www.google.com.br/maps/place/Lavyne+Boutique/@-3.7437057,-39.78347,18z/data=!4m15!1m8!3m7!1s0x7c0103637df6ac7:0xcf734ef1318514aa!2sR.+Vinte+e+Um+de+Junho+-+Irau%C3%A7uba,+CE,+62620-000!3b1!8m2!3d-3.7490082!4d-39.7827409!16s%2Fg%2F1ymtw5rx6!3m5!1s0x7c011a118a58fdb:0x4ce2ee2de1d67f5b!8m2!3d-3.7435067!4d-39.782615!16s%2Fg%2F11ptqd7gf2";
 });
 
-document.querySelector("#Share").addEventListener("click", async () => {
+document.querySelector("#Share-btn").addEventListener("click", async () => {
   try {
     await navigator.clipboard.writeText(
       "https://ericlescalvt.github.io/LavyneBoutique/"
